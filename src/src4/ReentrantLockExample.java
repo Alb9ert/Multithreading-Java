9c7faae5b13a19b570daf3ec081f2e81681b638d
Alb9ert <alberthansen2800@gmail.com>
2025-04-01 09:05:46 +0200

    src4

---
+++ src/src4/ReentrantLockExample.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockExample {
+    final ReentrantLock lock = new ReentrantLock();
+    private final Thread t1;
+    private final Thread t2;
+
+    public ReentrantLockExample() {
+        t1 = new Thread(new Worker());
+        t2 = new Thread(new Worker());
+    }
+
+    private void startThreads() {
+        t1.start();
+        t2.start();
+    }
+
+    public static void main(String[] args) {
+        ReentrantLockExample example = new ReentrantLockExample();
+        example.startThreads();
+    }
+
+    class Worker implements Runnable {
+        @Override
+        public void run() {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " is waiting for lock in method1");
+            someWork();
+            System.out.println(Thread.currentThread().getName() + " releasing lock in method1");
+            lock.unlock();
+        }
+
+        private void someWork() {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " is waiting for lock in method2");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                System.out.println(Thread.currentThread().getName() + " releasing lock in method2");
+                lock.unlock();
+            }
+        }
+    }
+}
